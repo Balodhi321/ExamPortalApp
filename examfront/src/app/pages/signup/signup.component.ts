a936c4d472953a954f8daef84e5701e0a734d46e
Shubham Balodhi <Shubham.Balodhi@motherson.com>
2025-01-28 07:20:20 +0530

    added validation and called backend apis

--- examfront/src/app/pages/signup/signup.component.ts
+++ examfront/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UserService,private snack:MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  public user ={
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
+
+  formSubmit() {
+    console.log(this.user);
+    if(this.user.username==''||this.user.password=='')
+    {
+      // alert('Please fill all the fields');
+      this.snack.open("Username and password is required !!", '',{
+        duration:2000,
+        verticalPosition:'top',
+        horizontalPosition:'right',
+      });
+      return;
+    }
+
+    this.userService.addUser(this.user).subscribe(
+      (data:any)=>{                             //for success
+        console.log(data);
+        console.log('this is working');
+        // alert('Success');
+        Swal.fire('Success !!','UserId is' + data.id,'success');
+      },
+      (error)=>{                              //for error
+        console.log(error);
+        // alert('Something went wrong');
+        this.snack.open("Something went wrong !!", '',{
+          duration:2000,
+          verticalPosition:'top',
+          horizontalPosition:'right',
+        });
+      }
+    )
   }
 
 }
---
+++ examfront/src/app/services/helper.ts
@@ -0,0 +1,3 @@
+// this file is made for storing urls
+let baseUrl = 'http://localhost:8080';
+export default baseUrl;
