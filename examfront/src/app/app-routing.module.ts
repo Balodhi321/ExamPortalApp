3b66c0a2c40d8eaa4c60108a8e08cfb2a936cc86
Shubham Balodhi <Shubham.Balodhi@motherson.com>
2025-01-29 09:23:13 +0530

    added login component

--- examfront/src/app/app-routing.module.ts
+++ examfront/src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
     pathMatch: 'full',
   }
 ];
